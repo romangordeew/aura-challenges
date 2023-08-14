d6b4b01e8b3ba60c02ab8eea7520f4073de3c228
0xeye <97349378+0xeye@users.noreply.github.com>
2023-08-14 17:12:44 +0100

    feat: add tasks per account

--- challenges/todo/Todo.sol
+++ challenges/todo/Todo.sol
@@ -1,6 +1,6 @@
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.21;
 
-// SPDX-License-Identifier: UNLICENSED
 contract Todo {
     struct Item {
         string text;
@@ -11,34 +11,33 @@ contract Todo {
     event TodoUpdated(uint256 indexed idx, string text);
     event TodoCompleted(uint256 indexed idx);
 
-    Item[] public items;
+    mapping(address => Item[]) public items;
 
     function getCount() public view returns (uint256) {
-        return items.length;
+        return items[msg.sender].length;
     }
 
     function create(string memory _text) public {
-        items.push(Item({
-            text: _text,
-            completed: false
-        }));
-        emit TodoCreated(items.length - 1, _text);
+        Item[] storage storedTasks = items[msg.sender];
+        Item memory item = Item(_text, false);
+        storedTasks.push(item);
+        emit TodoCreated(items[msg.sender].length - 1, _text);
     }
 
     function update(uint256 _idx, string memory _text) itemExists(_idx) public {
-        Item storage item = items[_idx];
+        Item storage item = items[msg.sender][_idx];
         item.text = _text;
         emit TodoUpdated(_idx, _text);
     }
 
     function toggleCompleted(uint256 _idx) itemExists(_idx) public {
-        Item storage item = items[_idx];
+        Item storage item = items[msg.sender][_idx];
         item.completed = !item.completed;
         emit TodoCompleted(_idx);
     }
 
     modifier itemExists(uint256 _idx) {
-        require(_idx < items.length, "Item does not exist");
+        require(_idx < items[msg.sender].length, "Item does not exist");
         _;
     }
 }
